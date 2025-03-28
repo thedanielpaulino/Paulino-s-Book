aed8e882c1003008cd6826ba75b2f7a772bc05fb
Daniel Paulino <160649011+thedanielpaulino@users.noreply.github.com>
2025-03-28 18:31:28 -0300

    Update UsuarioDAO.java

--- testetcc/app/src/main/java/com/example/testetcc/interfaces/UsuarioDAO.java
+++ testetcc/app/src/main/java/com/example/testetcc/interfaces/UsuarioDAO.java
@@ -1,6 +1,8 @@
 package com.example.testetcc.interfaces;
 
+import androidx.room.Dao;
 import androidx.room.Delete;
+import androidx.room.Insert;
 import androidx.room.Query;
 import androidx.room.Update;
 
@@ -8,31 +10,42 @@ import com.example.testetcc.model.Usuario;
 
 import java.util.List;
 
+
 // Interface com todos os metodos que irao interagir com o Banco de Dados
-public interface UsuarioDAO<User> {
+@Dao
+public interface UsuarioDAO {
 
     //Retorna todos os usuarios
-    @Query("SELECT * FROM Usuarios")
-    Usuario [] loadAllUsers();
+    @Query("SELECT * FROM usuarios")
+    Usuario[] loadAllUsers();
 
     //Retorna todos os id's dos Usuarios
     @Query("SELECT * FROM Usuarios WHERE id IN (:userIds)")
-    List<User> loadAllByIds(int[] userIds);
+    List<Usuario> loadAllByIds(int[] userIds);
 
     //Retorna os usuarios por seus apelidos
-    @Query("SELECT * FROM Usuarios WHERE apelido ")
-    List<User> loadAllapelido(String apelido);
+
+    @Query("SELECT * FROM usuarios WHERE email = :email AND senha = :senha LIMIT 1")
+    Usuario login(String email, String senha);
+    @Query("SELECT * FROM Usuarios WHERE apelido = :apelido ")
+    List<Usuario> loadAllApelido(String apelido);
 
     //Retorna o email de c ada usuario
-    @Query("SELECT * FROM Usuarios WHERE email")
-    List<User> loadALLemail(String email);
+    @Query("SELECT * FROM Usuarios WHERE email = :email")
+    List<Usuario> loadALLEmail(String email);
+
+    @Query("SELECT * FROM Usuarios WHERE senha = :senha")
+    List<Usuario> loadALLSenha(String senha);
 
     //Atualiza o Usuario
     @Update
-    void Update(Usuario usuario);
+    void update(Usuario usuario);
 
     // Exclui o um Usuario
+
+    @Insert
+    void inserir(Usuario usuario);
     @Delete
-    void delete(User user);
+    void delete(Usuario usuario);
 }
 
